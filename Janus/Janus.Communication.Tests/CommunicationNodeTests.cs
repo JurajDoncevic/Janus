f0d7d75f4c3e2c2f0825fe40ec968327ae424de2
JurajDoncevic <juraj.doncevic@fer.hr>
2022-03-23 12:55:17 +0100

    Register test added

--- Janus/Janus.Communication.Tests/CommunicationNodeTests.cs
+++ Janus/Janus.Communication.Tests/CommunicationNodeTests.cs
@@ -22,7 +22,7 @@ public class CommunicationNodeTests : IClassFixture<CommunicationNodeTestFixture
     }
 
     [Fact(DisplayName = "Test HELLO between 2 components")]
-    public void HelloBetweenTwoComponents()
+    public void SendHello()
     {
         using var mediator1 = CommunicationNodes.CreateTcpMediatorCommunicationNode(_mediatorCommunicationNodeOptions["Mediator1"]); 
         using var mediator2 = CommunicationNodes.CreateTcpMediatorCommunicationNode(_mediatorCommunicationNodeOptions["Mediator2"]);
@@ -33,10 +33,30 @@ public class CommunicationNodeTests : IClassFixture<CommunicationNodeTestFixture
         var resultRemotePoint = helloResult.Data;
         
         Assert.True(helloResult.IsSuccess);
-        Assert.Equal(mediator2.Options.NodeId, resultRemotePoint.Id);
+        Assert.Equal(mediator2.Options.NodeId, resultRemotePoint.NodeId);
         Assert.Equal(mediator2.Options.ListenPort, resultRemotePoint.Port);
         Assert.Empty(mediator1.RemotePoints);
         Assert.Empty(mediator2.RemotePoints);
+
+    }
+
+    [Fact(DisplayName = "Test Register using HELLO between 2 components")]
+    public void SendRegister()
+    {
+        using var mediator1 = CommunicationNodes.CreateTcpMediatorCommunicationNode(_mediatorCommunicationNodeOptions["Mediator1"]);
+        using var mediator2 = CommunicationNodes.CreateTcpMediatorCommunicationNode(_mediatorCommunicationNodeOptions["Mediator2"]);
+
+        var mediator2RemotePoint = new MediatorRemotePoint("127.0.0.1", mediator2.Options.ListenPort);
+        var mediator1RemotePoint = new MediatorRemotePoint(mediator1.Options.NodeId, "127.0.0.1", mediator1.Options.ListenPort);
+
+        var registerResult = mediator1.RegisterRemotePoint(mediator2RemotePoint).Result;
+        var resultRemotePoint = registerResult.Data;
+
+        Assert.True(registerResult.IsSuccess);
+        Assert.Equal(mediator2.Options.NodeId, resultRemotePoint.NodeId);
+        Assert.Equal(mediator2.Options.ListenPort, resultRemotePoint.Port);
+        Assert.Contains(resultRemotePoint, mediator1.RemotePoints);
+        Assert.Equal(mediator2.RemotePoints.First(), mediator1RemotePoint);
 
     }
 }
