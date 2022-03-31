13fe37d124f041eba22cd30b52034e20ada670ba
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2022-03-31 15:29:16 +0200

    idea for a SchemaModelBuilder

---
+++ Janus/Janus.Commons/SchemaModels/SchemaModelBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Janus.Commons.SchemaModels
+{
+    public class SchemaModelBuilder
+    {
+    }
+
+    public interface IDataSourceBuilder
+    {
+
+    }
+    public interface ISchemaBuilder
+    {
+
+    }
+    public interface ITableauBuilder
+    {
+
+    }
+    public interface IAttributeBuilder
+    {
+
+    }
+}
