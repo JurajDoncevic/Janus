9d7979ec28a0b3d5a5968dd73ef9c5e500bb3a60
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2022-04-02 12:54:30 +0200

    added string serialization for NodeTypes

--- Janus/Janus.Communication/Nodes/NodeTypes.cs
+++ Janus/Janus.Communication/Nodes/NodeTypes.cs
@@ -1,5 +1,8 @@
-﻿namespace Janus.Communication.Nodes;
+﻿using System.Text.Json.Serialization;
 
+namespace Janus.Communication.Nodes;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum NodeTypes
 {
     MASK_NODE,
