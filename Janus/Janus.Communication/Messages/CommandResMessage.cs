38124279d69769ae8f7bd5ac18326c24b8ec43ed
JurajDoncevic <juraj.doncevic@fer.hr>
2022-04-15 12:49:45 +0200

    fixed formatting; added JsonConstructor attributes

--- Janus/Janus.Communication/Messages/CommandResMessage.cs
+++ Janus/Janus.Communication/Messages/CommandResMessage.cs
@@ -1,28 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.Text.Json.Serialization;
 
-namespace Janus.Communication.Messages
+namespace Janus.Communication.Messages;
+
+public class CommandResMessage : BaseMessage
 {
-    public class CommandResMessage : BaseMessage
+    private readonly string _nodeId;
+
+    /// <summary>
+    /// Sender node's ID
+    /// </summary>
+    public string NodeId => _nodeId;
+
+    [JsonConstructor]
+    public CommandResMessage(string exchangeId, string nodeId) : base(exchangeId, Preambles.COMMAND_RESPONSE)
     {
-        private readonly string _nodeId;
+        _nodeId = nodeId;
+    }
 
-        /// <summary>
-        /// Sender node's ID
-        /// </summary>
-        public string NodeId => _nodeId;
-
-        public CommandResMessage(string exchangeId, string nodeId) : base(exchangeId, Preambles.COMMAND_RESPONSE)
-        {
-            _nodeId = nodeId;
-        }
-
-        public CommandResMessage(string nodeId) : base(Preambles.COMMAND_RESPONSE)
-        {
-            _nodeId = nodeId;
-        }
+    public CommandResMessage(string nodeId) : base(Preambles.COMMAND_RESPONSE)
+    {
+        _nodeId = nodeId;
     }
 }
--- Janus/Janus.Communication/Messages/QueryReqMessage.cs
+++ Janus/Janus.Communication/Messages/QueryReqMessage.cs
@@ -1,30 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
-namespace Janus.Communication.Messages
+namespace Janus.Communication.Messages;
+
+public class QueryReqMessage : BaseMessage
 {
-    public class QueryReqMessage : BaseMessage
+    private readonly string _nodeId;
+
+    /// <summary>
+    /// Sender node's ID
+    /// </summary>
+    public string NodeId => _nodeId;
+
+    [JsonConstructor]
+    public QueryReqMessage(string exchangeId, string nodeId) : base(exchangeId, Preambles.QUERY_REQUEST)
     {
-        private readonly string _nodeId;
+        _nodeId = nodeId;
+    }
 
-        /// <summary>
-        /// Sender node's ID
-        /// </summary>
-        public string NodeId => _nodeId;
-
-        [JsonConstructor]
-        public QueryReqMessage(string exchangeId, string nodeId) : base(exchangeId, Preambles.QUERY_REQUEST)
-        {
-            _nodeId = nodeId;
-        }
-
-        public QueryReqMessage(string nodeId) : base(Preambles.QUERY_REQUEST)
-        {
-            this._nodeId = nodeId;
-        }
+    public QueryReqMessage(string nodeId) : base(Preambles.QUERY_REQUEST)
+    {
+        this._nodeId = nodeId;
     }
 }
