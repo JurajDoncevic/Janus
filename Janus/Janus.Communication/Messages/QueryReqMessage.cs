0837c0136a13bbb501b21c42e9515c924d8d950b
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2022-05-18 16:48:13 +0200

    Added query model to query request

--- Janus/Janus.Communication/Messages/QueryReqMessage.cs
+++ Janus/Janus.Communication/Messages/QueryReqMessage.cs
@@ -1,4 +1,5 @@
 ﻿
+using Janus.Commons.QueryModels;
 using System.Text.Json.Serialization;
 
 namespace Janus.Communication.Messages;
@@ -8,20 +9,29 @@ namespace Janus.Communication.Messages;
 /// </summary>
 public class QueryReqMessage : BaseMessage
 {
+    private readonly Query _query;
+
+    /// <summary>
+    /// Requested query to be run
+    /// </summary>
+    public Query Query => _query;
+
     /// <summary>
     /// Constructor
     /// </summary>
     /// <param name="exchangeId">ID of the message exchange (request and its response)</param>
     /// <param name="nodeId">Sender's node ID</param>
     [JsonConstructor]
-    public QueryReqMessage(string exchangeId, string nodeId) : base(exchangeId, nodeId, Preambles.QUERY_REQUEST)
+    public QueryReqMessage(string exchangeId, string nodeId, Query query!!) : base(exchangeId, nodeId, Preambles.QUERY_REQUEST)
     {
+        _query = query;
     }
     /// <summary>
     /// Constructor
     /// </summary>
     /// <param name="nodeId">Sender's node ID</param>
-    public QueryReqMessage(string nodeId) : base(nodeId, Preambles.QUERY_REQUEST)
+    public QueryReqMessage(string nodeId, Query query!!) : base(nodeId, Preambles.QUERY_REQUEST)
     {
+        _query = query;
     }
 }
