d4648096ba6d4064f04d3e00c8281f2a5a1ceb99
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2022-06-12 17:11:36 +0200

    Initialized generic logger interface for libraries

---
+++ Janus/Janus.Utils/Logging/ILogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Janus.Utils.Logging;
+
+public interface ILogger
+{
+    void Info(string message);
+    void Warn(string message);
+    void Error(string message);
+    void Debug(string message);
+    void Critical(string message);
+    void Trace(string message);
+}
