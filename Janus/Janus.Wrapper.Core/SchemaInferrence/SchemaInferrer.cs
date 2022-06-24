aee12ab900e036a0e5934b54cefaeac3e56e6023
JurajDoncevic <juraj.doncevic@fer.hr>
2022-06-24 12:38:16 +0200

    added v2 inferrence

--- Janus/Janus.Wrapper.Core/SchemaInferrence/SchemaInferrer.cs
+++ Janus/Janus.Wrapper.Core/SchemaInferrence/SchemaInferrer.cs
@@ -16,7 +16,7 @@ public class SchemaInferrer
         _provider = schemaModelProvider;
     }
 
-    public Result<DataSource> InferSchemaModel()
+    public Result<DataSource> InferSchemaModel_v1()
         => ResultExtensions.AsResult<DataSource>(() =>
         {
             var dataSourceResult = _provider.GetDataSource();
@@ -59,4 +59,27 @@ public class SchemaInferrer
             }
             return null!;
         });
+
+    public Result<DataSource> InferSchemaModel_v2()
+    {
+        var result =
+        _provider.GetDataSource()
+            .Map(dataSource => (dataSource, dataSourceBuilder: SchemaModelBuilder.InitDataSource(dataSource.Name)))
+            .Map(r => _provider.GetSchemas().Match(x => x, s => new List<SchemaInfo>()).Fold(r.dataSourceBuilder, (schema, dataSourceBuilder) =>
+            {
+                return dataSourceBuilder.AddSchema(schema.Name, schemaBuilder => _provider.GetTableaus(schema.Name).Match(x => x, s => new List<TableauInfo>()).Fold(schemaBuilder, (tableau, sBuilder) =>
+                {
+                    return sBuilder.AddTableau(tableau.Name, tableauBuilder => _provider.GetAttributes(schema.Name, tableau.Name).Match(x => x, s => new List<AttributeInfo>()).Fold(tableauBuilder, (attribute, tBuilder) =>
+                    {
+                        return tBuilder.AddAttribute(attribute.Name, attributeBuilder => attributeBuilder.WithDataType(attribute.DataType)
+                                                                                                         .WithIsNullable(attribute.IsNullable)
+                                                                                                         .WithIsPrimaryKey(attribute.IsPrimaryKey)
+                                                                                                         .WithOrdinal(attribute.Ordinal));
+                    }));
+                }));
+            }))
+            .Bind(dataSourceBuilder => ResultExtensions.AsResult(() => dataSourceBuilder.Build()));
+
+        return result;
+    }
 }
