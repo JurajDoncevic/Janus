1c1d6fdddb314c2d63bbea2ad1f72338e2e437cf
JurajDoncevic <juraj.doncevic@fer.hr>
2022-06-28 16:25:16 +0200

    query translation ideas

--- Janus/Janus.Wrapper.CsvFiles/Querying/QueryTranslation.cs
+++ Janus/Janus.Wrapper.CsvFiles/Querying/QueryTranslation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Janus.Commons.SelectionExpressions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,4 +8,19 @@ using System.Threading.Tasks;
 namespace Janus.Wrapper.CsvFiles.Querying;
 public static class QueryTranslation
 {
+    private static HashSet<string> GetAllAttributeIdsInSelection(SelectionExpression selectionExpression)
+        => selectionExpression switch
+            {
+                AndOperator andOperator => GetAllAttributeIdsInSelection(andOperator.LeftOperand).Union(GetAllAttributeIdsInSelection(andOperator.RightOperand)).ToHashSet(),
+                OrOperator orOperator => GetAllAttributeIdsInSelection(orOperator.LeftOperand).Union(GetAllAttributeIdsInSelection(orOperator.RightOperand)).ToHashSet(),
+                NotOperator notOperator => GetAllAttributeIdsInSelection(notOperator.Operand),
+                LesserOrEqualThan lesserOrEqualThan => new HashSet<string> { lesserOrEqualThan.AttributeId },
+                LesserThan lesserThan => new HashSet<string> { lesserThan.AttributeId },
+                GreaterOrEqualThan greaterOrEqualThan => new HashSet<string> { greaterOrEqualThan.AttributeId },
+                GreaterThan greaterThan => new HashSet<string> { greaterThan.AttributeId },
+                NotEqualAs notEqualAs => new HashSet<string> { notEqualAs.AttributeId },
+                EqualAs equalAs => new HashSet<string> { equalAs.AttributeId },
+                _ => new HashSet<string>()
+            };
+
 }
