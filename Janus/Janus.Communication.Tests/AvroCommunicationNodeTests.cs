3ea175715f4b5d8798750572b3dddbfdf7dc3c7c
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2022-07-23 00:09:55 +0200

    Set communication node tests to run sequentially

--- Janus/Janus.Communication.Tests/AvroCommunicationNodeTests.cs
+++ Janus/Janus.Communication.Tests/AvroCommunicationNodeTests.cs
@@ -1,6 +1,9 @@
 ﻿using Janus.Communication.Tests.TestFixtures;
+using Xunit;
 
 namespace Janus.Communication.Tests;
+
+[Collection("Communication Node Tests")]
 public class AvroCommunicationNodeTests : CommunicationNodeTests<AvroCommunicationNodeTestFixture>
 {
     public AvroCommunicationNodeTests(AvroCommunicationNodeTestFixture testFixture) : base(testFixture)
--- Janus/Janus.Communication.Tests/BsonCommunicationNodeTests.cs
+++ Janus/Janus.Communication.Tests/BsonCommunicationNodeTests.cs
@@ -1,6 +1,9 @@
 ﻿using Janus.Communication.Tests.TestFixtures;
+using Xunit;
 
 namespace Janus.Communication.Tests;
+
+[Collection("Communication Node Tests")]
 public class BsonCommunicationNodeTests : CommunicationNodeTests<BsonCommunicationNodeTestFixture>
 {
     public BsonCommunicationNodeTests(BsonCommunicationNodeTestFixture testFixture) : base(testFixture)
