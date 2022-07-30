05447f46b95ec6a52843173fe37bc382e41d40b2
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2022-07-30 23:14:50 +0200

    Rollback on component design

--- Janus/Janus.Mediator.ConsoleApp/Options/ApplicationOptions.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Janus.Mediator.ConsoleApp.Options;
-public class ApplicationOptions
-{
-    public bool StartWithCLI { get; set; }
-}
--- Janus/Janus.Mediator.Core/Usings.cs
+++
@@ -1,4 +0,0 @@
-﻿global using FunctionalExtensions.Base;
-global using FunctionalExtensions.Base.Results;
-global using System;
-global using System.Collections.Generic;
