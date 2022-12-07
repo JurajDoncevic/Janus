dd795362b342a59fcbce0fc9265f59e1bc892d71
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2022-12-07 15:10:45 +0100

    initialized web apps for mediator and sqlite wrapper

---
+++ Janus/Janus.Mediator.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Janus.Mediator.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
