143cdac305515f7c2859eb14bb9253e84d370bb0
JurajDoncevic <juraj.doncevic@fer.hr>
2022-12-12 10:10:07 +0100

    Home page created

--- Janus/Janus.Mediator.WebApp/Controllers/HomeController.cs
+++ Janus/Janus.Mediator.WebApp/Controllers/HomeController.cs
@@ -2,20 +2,36 @@
 using Janus.Logging;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Janus.Mediator.WebApp.ViewModels;
 
 namespace Janus.Mediator.WebApp.Controllers;
 public class HomeController : Controller
 {
     private readonly Logging.ILogger<HomeController>? _logger;
-
-    public HomeController(Logging.ILogger? logger = null)
+    private readonly MediatorManager _mediatorManager;
+    private readonly MediatorOptions _mediatorOptions;
+    private readonly IConfiguration _configuration;
+    public HomeController(MediatorManager mediatorManager, MediatorOptions mediatorOptions, IConfiguration configuration, Logging.ILogger? logger = null)
     {
+        _mediatorManager = mediatorManager;
+        _mediatorOptions = mediatorOptions;
+        _configuration = configuration;
         _logger = logger?.ResolveLogger<HomeController>();
     }
 
     public IActionResult Index()
     {
-        return View();
+        var viewModel = new MediatorInfoViewModel()
+        {
+            NodeId = _mediatorOptions.NodeId,
+            CommunicationFormat = _mediatorOptions.CommunicationFormat,
+            ListenPort = _mediatorOptions.ListenPort,
+            NetworkAdapterType = _mediatorOptions.NetworkAdapterType,
+            PersistenceConnectionString = _mediatorOptions.PersistenceConnectionString,
+            TimeoutMs = _mediatorOptions.TimeoutMs,
+            WebPort = _configuration.GetSection("WebConfiguration").Get<WebConfiguration>().Port
+        };
+        return View(viewModel);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Janus/Janus.Mediator.WebApp/ViewModels/MediatorInfoViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Janus.Commons;
+using System.ComponentModel.DataAnnotations;
+
+namespace Janus.Mediator.WebApp.ViewModels;
+
+public class MediatorInfoViewModel
+{
+    [Display(Name = "Communication node id")]
+    public string NodeId { get; init; } = string.Empty;
+
+    [Display(Name = "Communication node listening port")]
+    public int ListenPort { get; init; }
+
+    [Display(Name = "Node's communication data format")]
+    public CommunicationFormats CommunicationFormat { get; init; }
+
+    [Display(Name = "Node's network adapter type")]
+    public NetworkAdapterTypes NetworkAdapterType { get; init; }
+    [Display(Name = "Timeout in milliseconds")]
+    public int TimeoutMs { get; init; }
+
+    [Display(Name = "Persistence connection string")]
+    public string PersistenceConnectionString { get; init; } = string.Empty;
+
+    [Display(Name = "Port for the web application")]
+    public int WebPort { get; init; }
+}
