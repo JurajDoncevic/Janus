509b516c26499641c610a3a2992bae99d94821a4
JurajDoncevic <juraj.doncevic@fer.hr>
2023-01-30 14:10:29 +0100

    changed enumerable type

--- Janus/Janus.Commons/CommandModels/Exceptions/MissingInstantiationAttributesException.cs
+++ Janus/Janus.Commons/CommandModels/Exceptions/MissingInstantiationAttributesException.cs
@@ -4,7 +4,7 @@ namespace Janus.Commons.CommandModels.Exceptions;
 
 public class MissingInstantiationAttributesException : Exception
 {
-    public MissingInstantiationAttributesException(List<string> unreferencedAttrs, TableauId tableauId)
+    public MissingInstantiationAttributesException(IEnumerable<string> unreferencedAttrs, TableauId tableauId)
         : base($"Attributes {string.Join(", ", unreferencedAttrs)} of tableau {tableauId} were not referenced in the instatiation clause")
     {
     }
