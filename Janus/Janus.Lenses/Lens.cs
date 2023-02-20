2ecc591e0b532899b384e894d5f7998a7e7bad90
JurajDoncevic <juraj.doncevic@fer.hr>
2023-02-20 12:30:57 +0100

    Some future Lens sketches

---
+++ Janus/Janus.Lenses/Lens.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Janus.Lenses;
+public abstract class Lens<TSource, TView>
+{
+
+    protected Lens()
+    {
+    }
+
+    public abstract Func<TView, TSource, TSource> Put { get; }
+
+    public abstract Func<TSource, TView> Get { get; }
+
+    /// <summary>
+    /// For a given source returns a view
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public TView CallGet(TSource source) => Get(source);
+
+    /// <summary>
+    /// For a given updated view and original source returns an updated source
+    /// </summary>
+    /// <param name="updatedView"></param>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public TSource CallPut(TView updatedView, TSource source) => Put(updatedView, source);
+
+    public override bool Equals(object? obj)
+    {
+        return obj is not null && 
+               obj is Lens<TSource, TView> lens &&
+               Get.Equals(lens.Get) &&
+               Put.Equals(lens.Put);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Get, Put);
+    }
+
+    public static bool operator ==(Lens<TSource, TView> left, Lens<TSource, TView> right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Lens<TSource, TView> left, Lens<TSource, TView> right)
+    {
+        return !(left == right);
+    }
+}
+
+
+public static class LensExtensions
+{
+    //public static Lens<TSource, RView> Compose<TSource, TView, RView>(this Lens<TSource, TView> lens1, Lens<TView, RView> lens2)
+    //    => Lens<TSource, TView>.Create<TSource, RView>(
+    //        s => lens2.Get(lens1.Get(s)),
+    //        (uv, s) => lens1.Put(lens2.Put(uv, lens1.Get(s)), s)
+    //        );
+}
---
+++ Janus/Janus.Lenses/RowDataDtoObjectLens.cs
@@ -0,0 +1,61 @@
+﻿using Janus.Commons.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Janus.Lenses;
+public class RowDataDtoObjectLens<TView> : Lens<RowData, TView>
+{
+    internal RowDataDtoObjectLens() : base()
+    {
+    }
+
+    public override Func<TView, RowData, RowData> Put =>
+        (view, originalSource) =>
+        {
+            var viewType = view?.GetType() ?? typeof(TView);
+
+            var columnInfos =
+                viewType.GetRuntimeProperties()
+                .Map(field => (name: field.Name, type: field.PropertyType))
+                .Map(t => (fieldName: t.name, fieldType: t.type, columnName: $"{columnNamePrefix}{t.name}"))
+                .ToDictionary(t => t.fieldName, t => t);
+
+            var columnDataTypes =
+                columnInfos.ToDictionary(t => t.Value.columnName, t => TypeMappings.MapToDataType(t.Value.fieldType));
+
+            var tblrBuilder = TabularDataBuilder.InitTabularData(columnDataTypes);
+
+            var rowData = new Dictionary<string, object?>();
+
+            foreach (var property in viewType.GetRuntimeProperties())
+            {
+                var value = property.GetValue(view);
+                var columnName = columnInfos[property.Name].columnName;
+                rowData.Add(columnName, value);
+            }
+
+            return RowData.FromDictionary(rowData);
+        };
+
+    public override Func<RowData, TView> Get =>
+        (source) =>
+        {
+            var viewType = typeof(TView);
+
+
+            var viewItem = Activator.CreateInstance<TView>();
+            foreach (var (colName, value) in source.ColumnValues.Map(t => (t.Key.Split('.').Last(), t.Value)))
+            {
+                string fieldName = $"_{colName}";
+
+                var targetField = viewType.GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+                targetField?.SetValue(viewItem, value);
+            }
+
+            return viewItem;
+        };
+}
