ce5773e879c57011a369719630ca22fde24b17d6
JurajDoncevic <juraj.doncevic@fer.hr>
2023-05-02 11:55:03 +0200

    added class var for command manager

--- Janus/Janus.Mask.LiteDB/LiteDbMaskManager.cs
+++ Janus/Janus.Mask.LiteDB/LiteDbMaskManager.cs
@@ -15,6 +15,7 @@ namespace Janus.Mask.LiteDB;
 public sealed class LiteDbMaskManager : MaskManager<LiteDbQuery, TableauId, Unit, Unit, Unit, LiteDbDelete, LiteDbInsert, LiteDbUpdate, Unit, Unit, Database, LiteDbData, BsonDocument>
 {
     private readonly LiteDbMaskQueryManager _queryManager;
+    private readonly LiteDbMaskCommandManager _commandManager;
     private readonly LiteDbMaskSchemaManager _schemaManager;
     private readonly ILogger<LiteDbMaskManager>? _logger;
     private readonly DatabaseMaterializer _databaseMaterializer;
@@ -23,6 +24,7 @@ public sealed class LiteDbMaskManager : MaskManager<LiteDbQuery, TableauId, Unit
     public LiteDbMaskManager(MaskCommunicationNode communicationNode, LiteDbMaskQueryManager queryManager, LiteDbMaskCommandManager commandManager, LiteDbMaskSchemaManager schemaManager, MaskPersistenceProvider persistenceProvider, LiteDbMaskOptions maskOptions, ILogger? logger = null) : base(communicationNode, queryManager, commandManager, schemaManager, persistenceProvider, maskOptions, logger)
     {
         _queryManager = queryManager;
+        _commandManager = commandManager;
         _schemaManager = schemaManager;
         _maskOptions = maskOptions;
         _logger = logger?.ResolveLogger<LiteDbMaskManager>();
