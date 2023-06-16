d8331f860c9fc03204490e6e30dc074c261059f6
Juraj Doncevic <juraj.doncevic@fer.hr>
2023-06-16 22:17:23 +0200

    ListHashSet lens added

---
+++ Janus/Janus.Lenses.Tests/Implementations/ListHashSetLensTests.cs
@@ -0,0 +1,12 @@
+﻿using Janus.Lenses.Implementations;
+
+namespace Janus.Lenses.Tests.Implementations;
+public sealed class ListHashSetLensTests : SymmetricLensTestingFramework<List<int>, HashSet<string>>
+{
+    protected override List<int> _x => new List<int> { 1, 1, 2, 3, 4, 5, 5, 6, 7, 7 };
+
+    protected override HashSet<string> _y => new HashSet<string> { "1", "2", "3", "4", "5", "6", "7" };
+
+    protected override SymmetricLens<List<int>, HashSet<string>> _lens 
+        => ListHashSetLenses.Construct(IntStringLenses.Construct());
+}
--- Janus/Janus.Lenses/Implementations/ListHashSetLens.cs
+++ Janus/Janus.Lenses/Implementations/ListHashSetLens.cs
@@ -1,38 +1,83 @@
 ﻿using Janus.Base;
+using System.Net.Http.Headers;
 
 namespace Janus.Lenses.Implementations;
-public class ListHashSetLens<LItem, RItem> : SymmetricLens<List<LItem>, HashSet<RItem>>
+public class ListHashSetLens<TLItem, TRItem> : SymmetricLens<List<TLItem>, HashSet<TRItem>>
 {
-    private readonly SymmetricLens<LItem, RItem> _itemLens;
-    protected override Result<List<LItem>> _CreateLeft(Option<HashSet<RItem>> right)
+    private readonly SymmetricLens<TLItem, TRItem> _itemLens;
+
+    internal ListHashSetLens(SymmetricLens<TLItem, TRItem> itemLens)
+    {
+        _itemLens = itemLens;
+    }
+
+    protected override Result<List<TLItem>> _CreateLeft(Option<HashSet<TRItem>> right)
         => Results.AsResult(() =>
             right.Match(
                 hashSet => hashSet.Fold(
-                    new List<Result<LItem>>(),
-                    (rItem, list) => list.Pass(lst => lst.Add(_itemLens.PutLeft(rItem, Option<LItem>.None)))
+                    new List<Result<TLItem>>(),
+                    (rItem, list) => list.Pass(l => l.Add(_itemLens.PutLeft(rItem, Option<TLItem>.None)))
                     ),
-                () => new List<Result<LItem>>()
+                () => new List<Result<TLItem>>()
                 )
             .AutoFold()
             .Map(Enumerable.ToList)
         );
 
-    protected override Result<HashSet<RItem>> _CreateRight(Option<List<LItem>> left)
-        => Results.AsResult(() => 
+    protected override Result<HashSet<TRItem>> _CreateRight(Option<List<TLItem>> left)
+        => Results.AsResult(() =>
             left.Match(
                 list => list.Fold(
-                    new HashSet<Result<RItem>>()
-                    )
+                    new HashSet<Result<TRItem>>(),
+                    (lItem, set) => set.Append(_itemLens.PutRight(lItem, Option<TRItem>.None)).ToHashSet()
+                    ),
+                () => new HashSet<Result<TRItem>>()
                 )
+            .AutoFold()
+            .Map(Enumerable.ToHashSet)
         );
 
-    protected override Result<List<LItem>> _PutLeft(HashSet<RItem> right, Option<List<LItem>> left)
+    protected override Result<List<TLItem>> _PutLeft(HashSet<TRItem> right, Option<List<TLItem>> left)
+        => Results.AsResult(() =>
+        {
+            var trivialView = CreateRight(left).Match(r => r, _ => new HashSet<TRItem>());
+            var positionMap = left.Value.Map(li =>
+                                            right
+                                            .Mapi((ridx, ri) => (ridx, ri))
+                                            .FirstOrDefault(rt => LensedEquals(rt.ri, li)))
+                                        .Mapi((lidx, t) => (lidx, t.ridx))
+                                        .ToDictionary(_ => (int)_.lidx, _ => (int)_.ridx);
+            var revMapping = (int setIdx) => positionMap.FirstOrDefault(kv => kv.Value == setIdx).Key;
+
+            var items = right.Mapi((ridx, ritem) => _itemLens.PutLeft(ritem, left.Map(ls => ls[revMapping((int)ridx)])));
+
+            var list = 
+                Enumerable.Range(0, positionMap.Count)
+                .Map(idx => items.ElementAt(positionMap[idx]))
+                .ToList();
+
+            return list
+                .AutoFold()
+                .Map(Enumerable.ToList);
+        });
+
+
+    protected override Result<HashSet<TRItem>> _PutRight(List<TLItem> left, Option<HashSet<TRItem>> right)
     {
-        throw new NotImplementedException();
+        return left.Map(li => _itemLens.PutRight(li, right.Map(rs => rs.FirstOrDefault(r => LensedEquals(r, li)))))
+                   .AutoFold()
+                   .Map(Enumerable.ToHashSet);
     }
 
-    protected override Result<HashSet<RItem>> _PutRight(List<LItem> left, Option<HashSet<RItem>> right)
-    {
-        throw new NotImplementedException();
-    }
+    private Result LensedEquals(TLItem lItem, TRItem rItem)
+        => _itemLens.PutRight(lItem, Option<TRItem>.Some(rItem)).Bind(li => li.Equals(rItem));
+
+    private Result LensedEquals(TRItem rItem, TLItem lItem)
+        => _itemLens.PutLeft(rItem, Option<TLItem>.Some(lItem)).Bind(ri => ri.Equals(lItem));
+}
+
+public static class ListHashSetLenses
+{
+    public static ListHashSetLens<TLItem, TRItem> Construct<TLItem, TRItem>(SymmetricLens<TLItem, TRItem> itemLens)
+        => new ListHashSetLens<TLItem, TRItem>(itemLens);
 }
