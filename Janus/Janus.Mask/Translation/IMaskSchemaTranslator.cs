79b7b3a7f78e6c0b025d95fa7081e68ed228668e
Juraj Doncevic <juraj.doncevic@fer.hr>
2023-07-24 23:46:30 +0200

    some simple fixes

--- Janus/Janus.Mask/Translation/IMaskSchemaTranslator.cs
+++ Janus/Janus.Mask/Translation/IMaskSchemaTranslator.cs
@@ -1,7 +1,9 @@
 ﻿using Janus.Commons.SchemaModels;
 using Janus.Components.Translation;
+using Janus.Mask.MaskedSchemaModel;
 
 namespace Janus.Mask.Translation;
 public interface IMaskSchemaTranslator<TMaskedSchema> : ISchemaTranslator<DataSource, TMaskedSchema>
+    where TMaskedSchema : MaskedDataSource
 {
 }
